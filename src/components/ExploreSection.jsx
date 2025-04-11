aafd42e1878b9165bbbf4fc4314c4596fce67891
Exigent07 <aravindhlearner07@gmail.com>
2025-04-11 13:03:01 +0530

    [+] Added files for all pages

---
+++ src/components/ExploreSection.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+import maskGroup2 from "./mask-group-2.png";
+import maskGroup3 from "./mask-group-3.png";
+import maskGroup4 from "./mask-group-4.png";
+import maskGroup5 from "./mask-group-5.png";
+import maskGroup from "./mask-group.png";
+import vector from "./vector.svg";
+
+export const ExploreSection = () => {
+  return (
+    <div className="relative w-[1440px] h-[1497px]">
+      <div className="absolute w-[1096px] h-[569px] top-[57px] left-[82px]">
+        <div className="absolute w-[648px] top-[38px] left-0 [font-family:'Open_Sans-Light',Helvetica] font-light text-white text-[120px] tracking-[0] leading-[120px]">
+          INNOVATE. CELEBRATE. DOMINATE.
+        </div>
+
+        <div className="absolute w-[468px] h-[494px] top-0 left-[628px]">
+          <div className="absolute w-[395px] h-[494px] top-0 left-[73px]">
+            <div className="absolute w-72 h-[432px] top-[31px] left-[54px] bg-black rounded-[10px] overflow-hidden rotate-[-15.89deg] shadow-[0px_10px_50px_10px_#00000080]">
+              <div className="relative h-[432px] [background:linear-gradient(0deg,rgba(0,0,0,0.2)_0%,rgba(0,0,0,0.2)_100%)]">
+                <div className="absolute w-72 h-[432px] top-0 left-0 [background:linear-gradient(180deg,rgba(0,0,0,0.3)_0%,rgba(0,0,0,0)_30%,rgba(0,0,0,0)_50%,rgba(0,0,0,0.5)_100%)]" />
+
+                <div className="absolute h-[34px] top-[346px] left-[30px] [text-shadow:0px_4px_7px_#000000b2] [font-family:'Open_Sans-Light',Helvetica] font-light text-[#ebe6d0] text-base tracking-[0] leading-[34px] whitespace-nowrap">
+                  Core
+                </div>
+
+                <div className="absolute h-[34px] top-[377px] left-7 [text-shadow:0px_4px_7px_#000000b2] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-[#ebe6d0] text-3xl tracking-[0] leading-[34px] whitespace-nowrap">
+                  Focus Areas
+                </div>
+
+                <div className="absolute w-[25px] h-[25px] top-[30px] left-60" />
+
+                <div className="absolute h-12 top-6 left-[18px] [text-shadow:0px_4px_7px_#000000b2] [font-family:'Open_Sans-Light',Helvetica] font-light text-[#ebe6d0] text-[35px] tracking-[0] leading-[normal]">
+                  VIDYUT
+                </div>
+
+                <div className="absolute h-12 top-[65px] left-5 [text-shadow:0px_4px_7px_#000000b2] [font-family:'Open_Sans-Light',Helvetica] font-light text-[#ebe6d0] text-[35px] tracking-[0] leading-[normal]">
+                  23K
+                </div>
+              </div>
+            </div>
+
+            <img
+              className="absolute w-[29px] h-[33px] top-[35px] left-60"
+              alt="Vector"
+              src={vector}
+            />
+          </div>
+
+          <div
+            className="w-[168px] h-px top-[466px] left-0 bg-white absolute object-cover"
+          />
+        </div>
+
+        <div
+          className="w-1.5 h-[102px] top-[467px] left-[625px] bg-white absolute object-cover"
+        />
+      </div>
+
+      <div className="absolute h-[200px] top-[741px] left-[63px] [font-family:'Open_Sans-ExtraBold',Helvetica] font-extrabold text-[#ffffff5e] text-[200px] tracking-[0] leading-[200px] whitespace-nowrap">
+        01
+      </div>
+
+      <div>
+        &lt;- -&gt;
+      </div>
+
+      <div className="flex w-[1107px] h-[591px] items-center gap-[34px] absolute top-[758px] left-[333px] overflow-hidden overflow-x-scroll">
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px] border border-solid border-white">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px] [background:linear-gradient(180deg,rgba(139,198,214,0.5)_0%,rgba(225,233,235,0.5)_100%)]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Matrix"
+                src="/images/matrix.png"
+              />
+            </div>
+
+            <div className="absolute w-[242px] h-16 top-[47px] left-[104px] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-white text-3xl text-center tracking-[0] leading-8">
+              Societal Transformation
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px]">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="[background:linear-gradient(180deg,rgba(75,133,87,0.5)_0%,rgba(72,122,80,0.5)_100%)] absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Mask group"group
+                src="/images/nature.png"
+              />
+            </div>
+
+            <div className="absolute w-[242px] h-16 top-12 left-[105px] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-white text-3xl text-center tracking-[0] leading-8">
+              Environment Care
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px]">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="[background:linear-gradient(180deg,rgba(63,75,136,0.5)_0%,rgba(153,219,231,0.5)_100%)] absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Mask group"
+                src="/images/science.png"
+              />
+            </div>
+
+            <div className="absolute w-[242px] h-16 top-12 left-[105px] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-white text-3xl text-center tracking-[0] leading-8">
+              Science &amp; Technology
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px]">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="[background:linear-gradient(180deg,rgba(251,94,163,0.5)_0%,rgba(189,79,3,0.5)_100%)] absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Mask group"
+                src="/images/culture"
+              />
+            </div>
+
+            <div className="absolute w-[111px] h-16 top-12 left-[171px] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-white text-3xl text-center tracking-[0] leading-8">
+              Arts &amp; Culture
+            </div>
+          </div>
+        </div>
+
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px]">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="[background:linear-gradient(180deg,rgba(158,135,121,0.5)_0%,rgba(0,79,74,0.5)_100%)] absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Mask group"
+                src={maskGroup4}
+              />
+            </div>
+
+            <p className="absolute w-[242px] h-16 top-12 left-[105px] [font-family:'Open_Sans-SemiBold',Helvetica] font-semibold text-white text-3xl text-center tracking-[0] leading-8">
+              Speak To Heal the World
+            </p>
+          </div>
+        </div>
+
+        <div className="relative w-[456px] h-[591px]">
+          <div className="relative w-[452px] h-[591px] bg-[#37373780] rounded-[30px]">
+            <div className="absolute w-[434px] h-[413px] top-[120px] left-[9px]">
+              <div className="[background:linear-gradient(180deg,rgba(39,66,83,0.5)_0%,rgba(98,141,163,0.5)_100%)] absolute w-[434px] h-96 top-0 left-0 rounded-[30px] blur-[56px]" />
+
+              <p className="absolute w-[386px] h-9 top-[377px] left-6 [font-family:'Open_Sans-Regular',Helvetica] font-normal text-white text-sm tracking-[0] leading-[18px]">
+                Empowering change, inspiring progress—transforming society for a
+                better tomorrow
+              </p>
+
+              <img
+                className="absolute w-[407px] h-72 top-[50px] left-3.5"
+                alt="Mask group"
+                src={maskGroup5}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
